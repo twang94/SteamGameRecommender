716c89ae6cc4fdca9549ef83322de3f17dfb39bb
tomwang <t.wang604@gmail.com>
2021-01-26 02:30:22 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# SteamGameRecommender
-A hybrid recommender for games on the Steam platform
+# Hybrid Recommendation Engine for Steam Games
+A hybrid recommender using LightFM for games on the Steam platform
+
+![source: ndtv.com](img/steamlogo.jpg)
+
+____
+# Background
+
+### Steam
+
+Steam is a digital video game distribution platform where members buy, download, and launch games all through a single application. The service recently hit an all-time record high number of concurrent users online at 25 million [(source)](https://www.gamesindustry.biz/articles/2020-12-14-steam-breaks-concurrent-users-record-at-nearly-25m).
+
+### Video Game Industry
+
+At the moment, the video game industry is surging, with global video game revenue expected to reach $180 billion in 2020.
+
+Due in large part to the Covid-19 pandemic, revenues from video games are expected to exceed many other forms of entertainment through 2021. However, even prior to the pandemic there was strong evidence suggesting videogames was experiencing a great growth period. [(source)](https://www.marketwatch.com/story/videogames-are-a-bigger-industry-than-sports-and-movies-combined-thanks-to-the-pandemic-11608654990#:~:text=Global%20videogame%20revenue%20is%20expected,North%20American%20sports%20industries%20combined)
+
+### Recommender Systems
+
+Recommendation engines are commonplace in many kinds of companies, especially those on digital platforms with huge item and user databases. Recommenders are crucial for guiding customers toward new and interesting products they may eventually purchase.
+<br><br>
+Due to these factors and my personal interest in gaming, I wanted to collect Steam data and build my own recommendation system for Steam games.
+
---
+++ src/download_funcs.py
@@ -0,0 +1,195 @@
+# modified code based on API work from https://nik-davis.github.io/posts/2019/steam-data-collection/
+
+# standard library imports
+import csv
+import datetime as dt
+import json
+import os
+import statistics
+import time
+
+def get_request(url, parameters=None):
+    """Return json-formatted response of a get request using optional parameters.
+    
+    Parameters
+    ----------
+    url : string
+    parameters : {'parameter': 'value'}
+        parameters to pass as part of get request
+    
+    Returns
+    -------
+    json_data
+        json-formatted response (dict-like)
+    """
+    try:
+        response = requests.get(url=url, params=parameters)
+    except SSLError as s:
+        print('SSL Error:', s)
+        
+        for i in range(5, 0, -1):
+            print('\rWaiting... ({})'.format(i), end='')
+            time.sleep(1)
+        print('\rRetrying.' + ' '*10)
+        
+        # recursively try again
+        return get_request(url, parameters)
+    
+    except MaxRetryError as m:
+        print('Max Retry Error:', m)
+        
+        for i in range(5, 0, -1):
+            print('\rWaiting... ({})'.format(i), end='')
+            time.sleep(1)
+        print('\rRetrying.' + ' '*10)
+        
+        # recursively try again
+        return get_request(url, parameters)
+    
+    if response:
+        return response.json()
+    else:
+        # response is none usually means too many requests. Wait and try again 
+        print('No response, waiting 30 seconds...')
+        time.sleep(30)
+        print('Retrying.')
+        return get_request(url, parameters)
+    
+def get_app_data(start, stop, parser, pause):
+    """
+    Return list of app data generated from parser.
+    
+    app_list : global applist variable
+    start: beginning index of applist
+    stop: end index of applist
+    pause: seconds to wait
+    parser : function to handle request
+    """
+    app_data = []
+    
+    # iterate through each row of app_list, confined by start and stop
+    for index, row in app_list[start:stop].iterrows():
+        print('Current index: {}'.format(index), end='\r')
+        
+        appid = row['appid']
+        name = row['name']
+
+        # retrive app data for a row, handled by supplied parser, and append to list
+        data = parser(appid, name)
+        app_data.append(data)
+
+        time.sleep(pause) # prevent overloading api with requests
+    
+    return app_data
+
+
+def process_batches(parser, app_list, download_path, data_filename, index_filename,
+                    columns, begin=0, end=-1, batchsize=100, pause=1):
+    """Process app data in batches, writing directly to file.
+    
+    parser : custom function to format request
+    app_list : dataframe of appid and name
+    download_path : path to store data
+    data_filename : filename to save app data
+    index_filename : filename to store highest index written
+    columns : column names for file
+    
+    Keyword arguments:
+    
+    begin : starting index (get from index_filename, default 0)
+    end : index to finish (defaults to end of app_list)
+    batchsize : number of apps to write in each batch (default 100)
+    pause : time to wait after each api request (default 1)
+    
+    returns: none
+    """
+    print('Starting at index {}:\n'.format(begin))
+    
+    # by default, process all apps in app_list
+    if end == -1:
+        end = len(app_list) + 1
+    
+    # generate array of batch begin and end points
+    batches = np.arange(begin, end, batchsize)
+    batches = np.append(batches, end)
+    
+    apps_written = 0
+    batch_times = []
+    
+    for i in range(len(batches) - 1):
+        start_time = time.time()
+        
+        start = batches[i]
+        stop = batches[i+1]
+        
+        app_data = get_app_data(start, stop, parser, pause)
+        
+        rel_path = os.path.join(download_path, data_filename)
+        
+        # writing app data to file
+        with open(rel_path, 'a', newline='', encoding='utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames=columns, extrasaction='ignore')
+            
+            for j in range(3,0,-1):
+                print("\rAbout to write data, don't stop script! ({})".format(j), end='')
+                time.sleep(0.5)
+            
+            writer.writerows(app_data)
+            print('\rExported lines {}-{} to {}.'.format(start, stop-1, data_filename), end=' ')
+            
+        apps_written += len(app_data)
+        
+        idx_path = os.path.join(download_path, index_filename)
+        
+        # writing last index to file
+        with open(idx_path, 'w') as f:
+            index = stop
+            print(index, file=f)
+            
+        # logging time taken
+        end_time = time.time()
+        time_taken = end_time - start_time
+        
+        batch_times.append(time_taken)
+        mean_time = statistics.mean(batch_times)
+        
+        est_remaining = (len(batches) - i - 2) * mean_time
+        
+        remaining_td = dt.timedelta(seconds=round(est_remaining))
+        time_td = dt.timedelta(seconds=round(time_taken))
+        mean_td = dt.timedelta(seconds=round(mean_time))
+        
+        print('Batch {} time: {} (avg: {}, remaining: {})'.format(i, time_td, mean_td, remaining_td))
+            
+    print('\nProcessing batches complete. {} apps written'.format(apps_written))
+    
+def reset_index(download_path, index_filename):
+    """Reset index in file to 0."""
+    rel_path = os.path.join(download_path, index_filename)
+    
+    with open(rel_path, 'w') as f:
+        print(0, file=f)
+        
+
+def get_index(download_path, index_filename):
+    """Retrieve index from file, returning 0 if file not found."""
+    try:
+        rel_path = os.path.join(download_path, index_filename)
+
+        with open(rel_path, 'r') as f:
+            index = int(f.readline())
+    
+    except FileNotFoundError:
+        index = 0
+        
+    return index
+
+
+def prepare_data_file(download_path, filename, index, columns):
+    """Create file and write headers if index is 0."""
+    if index == 0:
+        rel_path = os.path.join(download_path, filename)
+
+        with open(rel_path, 'w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=columns)
+            writer.writeheader()
